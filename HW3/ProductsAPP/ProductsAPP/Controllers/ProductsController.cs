2d2b62f77ab8dc21cd061b7be5579e9b1870e6c5
LHYi <402927348@qq.com>
2018-08-16 16:08:10 +0800

    change in Controller

--- HW3/ProductsAPP/ProductsAPP/Controllers/ProductsController.cs
+++ HW3/ProductsAPP/ProductsAPP/Controllers/ProductsController.cs
@@ -1,4 +1,4 @@
-using ProductsApp.Models;
+﻿using ProductsApp.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,17 +10,26 @@ namespace ProductsApp.Controllers
     public class ProductsController : ApiController
     {
         ChessBoard Board = new ChessBoard();
-        bool PlayerIsComputer = true;
+        readonly bool PlayerIsComputer = true;
+        public int Level = 0;
 
-        bool IsBlank(int x, int y)
+        void PutRenew()
         {
-            if (Board.XYCoordinate[x, y] == 0)
-                return true;
-            else
-                return false;
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Board.XYCoordinate[i, j]=0;
+                }
+            }
         }
 
-        int IsWin()
+        void PutSetLevel(int Choice)
+        {
+            Level = Choice;
+        }
+
+        int GetIsWin()
         {           
             int ComputerWin = 1;
             int ManWin = 2;
@@ -58,7 +67,7 @@ namespace ProductsApp.Controllers
             {
                 for (int j = 0; j < 3; j++)
                 {
-                    if (IsBlank(i,j))
+                    if (Board.IsBlank(i,j))
                     {
                         return GoOn;
                     }
@@ -67,223 +76,25 @@ namespace ProductsApp.Controllers
             return Draw;
         }
 
-        void RecordMove(int x, int y,bool player)
-        {
-            //Computer=1 Man=0
-            if (player)
-            {
-                Board.XYCoordinate[x, y] = 1;
-            }
-            else
-                Board.XYCoordinate[x, y] = -1;
-        }
-
-        int GetRandomMove()
-        {
-            Random ra = new Random(unchecked((int)DateTime.Now.Ticks));
-            int x = ra.Next(0, 2);
-            return x;
-        }
-
-        void EasyMove()
-        {
-            
-            int x = 0;
-            int y = 0;
-            while(true)
-            {
-                x = GetRandomMove();
-                y = GetRandomMove();
-                if (IsBlank(x, y, Board.XYCoordinate))
-                {
-                    Board.XYCoordinate[x, y] = 1;
-                    break;
-                }
-            }
-            RecordMove(x, y, PlayerIsComputer);
-        }
-
-        void FillTheOnlyBlankInLine(int a, int b, int c)
-        {
-            if (a != 0 && b != 0 && c == 0)
-            {
-                c = 1;
-            }
-            else if (a != 0 && b == 0 && c != 0)
-            {
-                b = 1;
-            }
-            else if (a == 0 && b != 0 && c != 0)
-            {
-                a = 1;
-            }
-        }
-
-        bool Defense()
-        {           
-            for(int i = 0; i < 3; i++)
-            {
-                int count = 0;
-                for (int j = 0; j < 3; j++)
-                {
-                    count += Board.XYCoordinate[i, j];
-                }
-                if(count == -2)
-                {
-                    FillTheOnlyBlankInLine(Board.XYCoordinate[i, 0], Board.XYCoordinate[i, 1], Board.XYCoordinate[i, 2]);
-                    return true;
-                }
-            }
-            for (int i = 0; i < 3; i++)
-            {
-                int count = 0;
-                for (int j = 0; j < 3; j++)
-                {
-                    count += Board.XYCoordinate[j, i];
-                }
-                if (count == -2)
-                {
-                    FillTheOnlyBlankInLine(Board.XYCoordinate[0,i], Board.XYCoordinate[1,i], Board.XYCoordinate[2,i]);
-                    return true;
-                }
-            }
-            if(Board.XYCoordinate[0,0] + Board.XYCoordinate[1,1] + Board.XYCoordinate[2,2] == -2)
-            {
-                FillTheOnlyBlankInLine(Board.XYCoordinate[0, 0], Board.XYCoordinate[1, 1], Board.XYCoordinate[2, 2]);
-                return true;
-            }
-            if (Board.XYCoordinate[0, 2] + Board.XYCoordinate[1, 1] + Board.XYCoordinate[2, 0] == -2)
-            {
-                FillTheOnlyBlankInLine(Board.XYCoordinate[0, 2], Board.XYCoordinate[1, 1], Board.XYCoordinate[2, 0]);
-                return true;
-            }
-            return false;
-        }
-
-        bool Attack()
-        {
-            for (int i = 0; i < 3; i++)
-            {
-                int count = 0;
-                for (int j = 0; j < 3; j++)
-                {
-                    count += Board.XYCoordinate[i, j];
-                }
-                if (count == 2)
-                {
-                    FillTheOnlyBlankInLine(Board.XYCoordinate[i, 0], Board.XYCoordinate[i, 1], Board.XYCoordinate[i, 2]);
-                    return true;
-                }
-            }
-            for (int i = 0; i < 3; i++)
-            {
-                int count = 0;
-                for (int j = 0; j < 3; j++)
-                {
-                    count += Board.XYCoordinate[j, i];
-                }
-                if (count == 2)
-                {
-                    FillTheOnlyBlankInLine(Board.XYCoordinate[0, i], Board.XYCoordinate[1, i], Board.XYCoordinate[2, i]);
-                    return true;
-                }
-            }
-            if (Board.XYCoordinate[0, 0] + Board.XYCoordinate[1, 1] + Board.XYCoordinate[2, 2] == 2)
-            {
-                FillTheOnlyBlankInLine(Board.XYCoordinate[0, 0], Board.XYCoordinate[1, 1], Board.XYCoordinate[2, 2]);
-                return true;
-            }
-            if (Board.XYCoordinate[0, 2] + Board.XYCoordinate[1, 1] + Board.XYCoordinate[2, 0] == 2)
-            {
-                FillTheOnlyBlankInLine(Board.XYCoordinate[0, 2], Board.XYCoordinate[1, 1], Board.XYCoordinate[2, 0]);
-                return true;
-            }
-            return false;
-        }
-
-        void HardMove()
-        {
-            if (Attack())
-            {
-                return;
-            }
-            else if (Defense())
-            {
-                return;
-            }            
-            else
-            {
-                EasyMove();
-            }
-        }
-
-        int[] Transform()
-        {
-            int[] TempBoard = { 0, 0, 0, 0, 0, 0, 0, 0, 0 };
-            int counter = 0;
-            for (int i = 0; i < 3; i++)
-            {
-                for (int j = 0; j < 3; j++)
-                {
-                    TempBoard[counter] = Board.XYCoordinate[i, j];
-                    counter++;
-                }
-            }
-            return TempBoard;
-        }
-
-        void InvertTransform( int[] TempBoard)
-        {
-            int counter = 0;
-            for (int i = 0; i < 3; i++)
-            {
-                for (int j = 0; j < 3; j++)
-                {
-                    Board.XYCoordinate[i, j] = TempBoard[counter];
-                    counter++;
-                }
-            }
-        }
-        void HellMove()
+        void PutRecordMove(int x, int y)
         {            
-            int[] Angle = { 0, 2, 6, 8 };
-            int[] Side = { 1, 3, 5, 7 };
-
-            if (Attack())
+            Board.XYCoordinate[x, y] = -1;
+        }                                                       
+        
+        void PutMove()
+        {
+            if(Level==0)
             {
-                return;
+                Board.EasyMove();
             }
-            else if (Defense())
+            else if(Level==1)
             {
-                return;
+                Board.HardMove();
             }
-            else
+            else if(Level==2)
             {
-                int[] TempBoard = Transform();
-                for (int i = 0; i < 4; i++)
-                {
-                    if (TempBoard[Angle[i]] == 0)
-                    {
-                        TempBoard[Angle[i]] = 1;
-                        InvertTransform(TempBoard);
-                        return;
-                    }
-                }
-                if (TempBoard[4] == 0)
-                {
-                    TempBoard[4] = 1;
-                    InvertTransform(TempBoard);
-                }                    
-                for (int i = 0; i < 4; i++)
-                {
-                    if (TempBoard[Side[i]] == 0)
-                    {
-                        TempBoard[Side[i]] = 1;
-                        InvertTransform(TempBoard);
-                        return;
-                    }
-                }
-            }            
+                Board.HellMove();
+            }
         }
 
     }
